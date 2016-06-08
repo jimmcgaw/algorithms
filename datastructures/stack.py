31d1ef08070e920c1935c0bca2450824e83c5e03
jimmcgaw <jpmcgaw@gmail.com>
2016-06-08 09:21:46 -0700

    silly stack implementation with unit tests

--- datastructures/stack.py
+++ datastructures/stack.py
@@ -1,2 +1,36 @@
 class Stack(object):
-    pass
+    def __init__(self):
+        """ we use a list to track internal state. Kind of
+        odd since Python lists implement push (aka append) and pop operations
+        that manipulate the *end* of the list. This implementation hacks
+        it up to manipulate the *start* of the list.
+
+        """
+        self.items = []  # list for tracking internal stack state
+
+    def __len__(self):
+        return len(self.items)
+
+    def top(self):
+        """ return the value of the last item pushed onto the stack
+        If the stack is empty, returns False.
+        """
+        if len(self) == 0:
+            return False
+        else:
+            return self.items[0]
+
+    def push(self, item):
+        """ insert an item at the front of our list, at index 0. """
+        self.items.insert(0, item)
+        return self.items
+
+    def pop(self):
+        """ returns the value of the last item pushed onto the stack,
+        and removes this value from the stack. If stack is empty, returns
+        'underflow'.
+        """
+        if len(self) == 0:
+            return "underflow"
+        else:
+            return self.items.pop(0)
--- datastructures/test_stack.py
+++ datastructures/test_stack.py
@@ -4,7 +4,36 @@ import unittest
 
 class TestStack(unittest.TestCase):
     def setUp(self):
-        pass
+        self.stack = Stack()
+
+    def test_top_empty_stack(self):
+        latest_item = self.stack.top()
+        self.assertEqual(latest_item, False)
+
+    def test_top_with_stack_contents(self):
+        items_to_push = [5,9,22]
+        for i in items_to_push:
+            self.stack.push(i)
+        self.assertEqual(self.stack.top(), 22)
+        self.assertEqual(len(self.stack), len(items_to_push))
 
     def test_push(self):
-        pass
+        stack_contents = self.stack.push(1)
+        self.assertEqual(len(self.stack), 1)
+        self.assertEqual(stack_contents, [1])
+
+    def test_pop_with_stack_contents(self):
+        items_to_push = [5,9,22]
+        for i in items_to_push:
+            self.stack.push(i)
+
+        popped_item = self.stack.pop()
+        self.assertEqual(popped_item, items_to_push[len(items_to_push)-1])
+        self.assertEqual(len(self.stack), len(items_to_push)-1)
+
+    def test_pop_with_empty_stack(self):
+        popped_item = self.stack.pop()
+        self.assertEqual(popped_item, "underflow")
+
+if __name__ == '__main__':
+    unittest.main()
